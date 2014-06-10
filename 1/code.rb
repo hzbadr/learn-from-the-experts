e159a263b9b881dad19b39c742093c2ada4f0026
Hassan Zaki <hassan.zaki@badrit.com>
2014-06-10 23:27:45 +0300

    * scope

---
+++ 1/code.rb
@@ -0,0 +1,19 @@
+class A
+  a = '1'
+  class << self
+    puts a
+  end
+end
+# NameError: undefined local variable or method `x' for #<Class:A>
+
+class B
+  @b = 1
+  class << self
+    puts @b
+  end
+end
+
+# empty result
+
+
+# =======================================================================
